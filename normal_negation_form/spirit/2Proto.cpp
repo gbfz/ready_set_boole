73de48e58a7862a51bf2f73732561241e73f9d8c
George-Ber Zaltsman <berzaltsman@gmail.com>
2022-09-27 03:01:42 +0300

    yeah baby i've got a working parser now

--- normal_negation_form/spirit/2Proto.cpp
+++ normal_negation_form/spirit/2Proto.cpp
@@ -25,50 +25,56 @@ namespace ast {
 
 } // namespace ast
 
+namespace parser::utils {
+
+	using x3::_val;
+	using x3::_attr;
+
+} // namespace parser::utils
+
 namespace parser {
 
 	using x3::ascii::char_;
 
-	x3::rule<class BinaryOperator>		binary_operator		= "binary operator";
-	x3::rule<class UnaryOperator>		unary_operator		= "unary operator";
 	x3::rule<class Primitive>			primitive			= "primitive";
-	x3::rule<class BinaryOperation> 	binary_operation	= "binary operation";
-	x3::rule<class UnaryOperation>		unary_operation		= "unary operation";
+	x3::rule<class UnaryOperator>		unary_operator		= "unary operator";
+	x3::rule<class BinaryOperator>		binary_operator		= "binary operator";
 	x3::rule<class CompoundOperation>	compound_operation	= "compound operation";
 
-	const auto binary_operator_def		= char_('&') | char_('|') | char_('^') | char_('>') | char_('=');
-	const auto unary_operator_def		= char_('!');
 	const auto primitive_def			= char_('1') | char_('0');
+	const auto unary_operator_def		= char_('!');
+	const auto binary_operator_def		= char_('&') | char_('|') | char_('^') | char_('>') | char_('=');
 
-	const auto binary_operation_def		= (primitive >> primitive >> binary_operator);
-	const auto unary_operation_def		= (primitive >> unary_operator);
+	const auto compound_operation_def	= (primitive >> primitive >> binary_operator)
+									 >> *((primitive >> binary_operator) | unary_operator);
 
-	const auto compound_operation_def	= binary_operation >> *(unary_operation | unary_operator);
-
-
-	x3::rule<class node, ast::node>		operation			= "operation";
-	x3::rule<class node_value>			operation_value		= "operation value";
-	x3::rule<class node_key_value>		operation_key_value	= "operation key value";
+	x3::rule<class Node, ast::node>		operation			= "operation";
+	x3::rule<class NodeValue>			operation_value		= "operation value";
+	x3::rule<class NodeKeyValue>		operation_key_value	= "operation key value";
+	x3::rule<class Start, ast::node>	start				= "start";
 
 	const auto operation_value_def		= compound_operation | operation;
 	const auto operation_key_value_def	= compound_operation >> *operation_value;
 	const auto operation_def			= +operation_key_value;
+	const auto start_def				= (primitive >> unary_operator) | operation;
 
-	BOOST_SPIRIT_DEFINE(binary_operator, unary_operator,
-						primitive, binary_operation, unary_operation,
-						compound_operation,
+	BOOST_SPIRIT_DEFINE(start,
+						binary_operator, unary_operator,
+						primitive, compound_operation,
 						operation_value, operation_key_value, operation);
 
 } // namespace parser
 
 int main()
 {
-	// std::string s = "11|!!!1&";
 	std::string s = "1!";
+	// std::string s = "11!";
+	// std::string s = "11|!!!1&1|1=";
+	// std::string s = "11|1!";
 	// int yeah;
 	ast::node yeah;
 	auto iter = s.begin();
-	bool r = boost::spirit::x3::parse(iter, s.end(), parser::operation, yeah);
+	bool r = boost::spirit::x3::parse(iter, s.end(), parser::start, yeah);
 	if (r && iter == s.end())
 	{
 		std::cout << "-------------------------\n";
