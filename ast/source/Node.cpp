8ef515362dbd5df24e138d7bf8849fadb63410ff
George-Ber Zaltsman <berzaltsman@gmail.com>
2022-10-21 03:46:15 +0300

    generate cool ast lib

--- ast/source/Node.cpp
+++ ast/source/Node.cpp
@@ -1,33 +1,28 @@
 #include "ASTree.hpp"
 
-namespace AST {
+namespace ast {
 
-void Node::add_child(int value)
+tree::tree(int value) : value(value) {}
+
+tree::tree(const tree& other) : std::vector<ast::tree>(), value(other.value)
 {
-	emplace_back(value);
+	assign(other.begin(), other.end());
 }
 
-Node& Node::fst_child()
+tree& tree::operator= (const tree& other)
 {
-	return front();
+	value = other.value;
+	assign(other.begin(), other.end());
+	return *this;
 }
 
-const Node& Node::fst_child() const
-{
-	return front();
-}
+tree& tree::fst_child() { return front(); }
+const tree& tree::fst_child() const { return front(); }
 
-Node& Node::snd_child()
-{
-	return back();
-}
+tree& tree::snd_child() { return back(); }
+const tree& tree::snd_child() const { return back(); }
 
-const Node& Node::snd_child() const
-{
-	return back();
-}
-
-bool Node::exec() const
+bool tree::exec() const
 {
 	switch (value)
 	{
@@ -35,35 +30,41 @@ bool Node::exec() const
 		case '&': return fst_child().exec() && snd_child().exec();
 		case '|': return fst_child().exec() || snd_child().exec();
 		case '^': return fst_child().exec() != snd_child().exec();
+		case '=': return fst_child().exec() == snd_child().exec();
 		case '>': if (!fst_child().exec())
 					  return true;
 				  return snd_child().exec();
-		case '=': return fst_child().exec() == snd_child().exec();
 	}
 	return value == '1';
 }
 
-/*
-void Node::printValue(const std::string& pref, char value, Branch branch)
+std::string treeToString(const tree& tree, std::string acc)
 {
-	static const auto resetColor = [] { return "\033[0m"; };
-	static const auto chooseColor = []()
+	acc = char(tree.value) + acc;
+	std::string fst, snd;
+	switch (tree.size())
 	{
-		static auto i = 0u;
-		static const std::string colors[] = { "\033[1;32m", "\033[1;33m", "\033[1;31m", "\033[1;35m" };
-		return colors[i++ % 4];
-	};
-	// std::cout << prefix << (isRight ? "⦦ " : "⦧ " )
-	std::cout << pref
-			  << (branch == Branch::right ? "⦧ " : "⦦ " )
-			  << chooseColor()
-			  << '\'' << char(value) << '\''
-			  << resetColor() << '\n';
+		case 1: fst = treeToString(tree.fst_child());
+				return fst + acc;
+		case 2: fst = treeToString(tree.snd_child());
+				snd = treeToString(tree.fst_child());
+				return fst + snd + acc;
+	}
+	return acc;
 }
-*/
 
-/*
-void Node::printValue(const std::string& prefix, char value, bool isRight)
+bool operator== (const tree& lhs, const tree& rhs)
+{
+	if (rhs.value == placeholder || lhs.value == placeholder)
+		return true;
+	if (lhs.size() == 1 && rhs.size() == 1 && lhs.value == rhs.value)
+		return lhs.fst_child() == rhs.fst_child();
+	if (lhs.size() == 2 && rhs.size() == 2 && lhs.value == rhs.value)
+		return lhs.fst_child() == rhs.fst_child() && lhs.snd_child() == rhs.snd_child();
+	return lhs.value == rhs.value;
+}
+
+void tree::printValue(const std::string& pref, char value, bool isRight)
 {
 	static const auto resetColor = [] { return "\033[0m"; };
 	static const auto chooseColor = []()
@@ -72,24 +73,27 @@ void Node::printValue(const std::string& prefix, char value, bool isRight)
 		static const std::string colors[] = { "\033[1;32m", "\033[1;33m", "\033[1;31m", "\033[1;35m" };
 		return colors[i++ % 4];
 	};
-	std::cout << prefix << (isRight ? "⦧ " : "⦦ " )
-	// std::cout << prefix << (isRight ? "⦦ " : "⦧ " )
+	std::cout << pref
+			  << (isRight ? "⦧ " : "⦦ " )
 			  << chooseColor() << '\'' << char(value) << '\'' << resetColor() << '\n';
 }
-*/
 
-void Node::printValue(int tab, char value)
+void printTree(const std::string& pref, const tree& node, bool isRight)
 {
-	static const auto resetColor = [] { return "\033[0m"; };
-	static const auto chooseColor = [](int tab)
+	switch (node.size())
 	{
-		static const char* colors[] = { "\033[1;36m", "\033[1;34m", "\033[1;33m", "\033[1;32m", "\033[1;35m" };
-		return colors[tab % 5];
-	};
-	std::cout << chooseColor(tab)
-			  << std::string(tab, ' ')
-			  << char(value)
-			  << resetColor() << '\n';
+		case 0: return tree::printValue(pref, node.value, isRight);
+		case 1: tree::printValue(pref, node.value, isRight);
+				return printTree(pref + (isRight ? "ᐧ  " : "   "), node.fst_child(), false);
+		case 2: printTree(pref + (isRight ? "   " : "ᐧ  "), node.fst_child(), true);
+				tree::printValue(pref, node.value, isRight);
+				printTree(pref + (isRight ? "ᐧ  " : "   "), node.snd_child(), false);
+	}
 }
 
-} // namespace AST
+void printTree(const tree& node)
+{
+	printTree("", node, false);
+}
+
+} // namespace ast
