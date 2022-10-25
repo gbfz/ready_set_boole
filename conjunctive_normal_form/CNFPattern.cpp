85cbe1bff0881c32b1c08581e2972f01c7f3a86e
George-Ber Zaltsman <berzaltsman@gmail.com>
2022-10-25 21:08:32 +0300

    i think im done

--- conjunctive_normal_form/CNFPattern.cpp
+++ conjunctive_normal_form/CNFPattern.cpp
@@ -2,10 +2,9 @@
 
 namespace cnf::pattern {
 
-// Distributivity 1 | conOverDis: AB|C& ≡ AB|AC|&
-// Distributivity 2 | disOverCon: AB&C| ≡ AB&AC&|
+// AB&C| ≡ CA|CB|&
 
-const ast::tree disOverConPattern()
+const ast::tree docPatternR()
 {
 	ast::tree pattern('|');
 	pattern.add_two(ast::placeholder, '&');
@@ -13,14 +12,75 @@ const ast::tree disOverConPattern()
 	return pattern;
 }
 
-ast::tree& rewriteDisOverCon(ast::tree& tree)
+ast::tree& rewriteDocL(ast::tree& tree)
 {
-	auto a = tree.fst_child();
+	auto c = tree.snd_child();
+	auto b = tree.fst_child().fst_child();
+	auto a = tree.fst_child().snd_child();
+	tree = ast::tree('&').add_two('|', '|');
+	tree.fst_child().add_two(b, c);
+	tree.snd_child().add_two(a, c);
+	return tree;
+}
+
+// ABC&| ≡ CA|CB|&
+
+const ast::tree docPatternL()
+{
+	ast::tree pattern('|');
+	pattern.add_two('&', ast::placeholder);
+	pattern.fst_child().add_two(ast::placeholder, ast::placeholder);
+	return pattern;
+}
+
+ast::tree& rewriteDocR(ast::tree& tree)
+{
+	auto c = tree.fst_child();
 	auto b = tree.snd_child().fst_child();
-	auto c = tree.snd_child().snd_child();
-	tree = ast::tree('|').add_two('&', '&');
-	tree.fst_child().add_two(c, a);
-	tree.snd_child().add_two(b, a);
+	auto a = tree.snd_child().snd_child();
+	tree = ast::tree('&').add_two('|', '|');
+	tree.fst_child().add_two(b, c);
+	tree.snd_child().add_two(a, c);
+	return tree;
+}
+
+// AB|C| ≡ ABC||
+
+const ast::tree doubleDisjunctionPattern()
+{
+	ast::tree pattern('|');
+	pattern.add_two(ast::placeholder, '|');
+	pattern.snd_child().add_two(ast::placeholder, ast::placeholder);
+	return pattern;
+}
+
+ast::tree& rewriteDoubleDisjunction(ast::tree& tree)
+{
+	auto c = tree.fst_child();
+	auto b = tree.snd_child().fst_child();
+	auto a = tree.snd_child().snd_child();
+	tree = ast::tree('|').add_two('|', a);
+	tree.fst_child().add_two(c, b);
+	return tree;
+}
+
+// AB&C& ≡ ABC&&
+
+const ast::tree doubleConjunctionPattern()
+{
+	ast::tree pattern('&');
+	pattern.add_two(ast::placeholder, '&');
+	pattern.snd_child().add_two(ast::placeholder, ast::placeholder);
+	return pattern;
+}
+
+ast::tree& rewriteDoubleConjunction(ast::tree& tree)
+{
+	auto c = tree.fst_child();
+	auto b = tree.snd_child().fst_child();
+	auto a = tree.snd_child().snd_child();
+	tree = ast::tree('&').add_two('&', a);
+	tree.fst_child().add_two(c, b);
 	return tree;
 }
 
--- conjunctive_normal_form/CNFPattern.hpp
+++ conjunctive_normal_form/CNFPattern.hpp
@@ -2,6 +2,17 @@
 #include "ASTree.hpp"
 
 namespace cnf::pattern {
-	const ast::tree disOverConPattern();
-	ast::tree& rewriteDisOverCon(ast::tree& tree);
+
+	// doc  ⇔  disjunction over conjunction
+	const ast::tree docPatternL();
+	ast::tree& rewriteDocL(ast::tree& tree);
+
+	const ast::tree docPatternR();
+	ast::tree& rewriteDocR(ast::tree& tree);
+
+	const ast::tree doubleDisjunctionPattern();
+	ast::tree& rewriteDoubleDisjunction(ast::tree& tree);
+
+	const ast::tree doubleConjunctionPattern();
+	ast::tree& rewriteDoubleConjunction(ast::tree& tree);
 }
