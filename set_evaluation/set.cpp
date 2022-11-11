48a0b59396498ef6aa9ae2cd7b9b5a5aac48a847
George-Ber Zaltsman <berzaltsman@gmail.com>
2022-11-12 01:28:33 +0200

    some cleanup before organizing a class

--- set_evaluation/set.cpp
+++ set_evaluation/set.cpp
@@ -11,54 +11,6 @@ using set = std::set<int>;
 
 set U;
 
-template <class T>
-void p(T cont)
-{
-	if constexpr ( std::is_same_v<T, std::vector<char>>
-				|| std::is_same_v<T, std::set<char>>
-				|| std::is_same_v<T, std::deque<char>>)
-	{
-		std::cout << "{ ";
-		for (char c : cont)
-			std::cout << c << ' ';
-		std::cout << '}';
-	}
-	if constexpr ( std::is_same_v<T, std::vector<int>>
-				|| std::is_same_v<T, std::deque<int>>
-				|| std::is_same_v<T, set>)
-	{
-		std::cout << "{ ";
-		for (int i : cont)
-			std::cout << i << ' ';
-		std::cout << '}';
-	}
-	if constexpr ( std::is_same_v<T, std::vector<std::vector<int>>>
-				|| std::is_same_v<T, std::deque<set>>
-				|| std::is_same_v<T, std::vector<set>>)
-	{
-		std::cout << "{ ";
-		for (const auto& v : cont)
-		{
-			std::cout << "{ ";
-			for (int i : v)
-				std::cout << i << ' ';
-			std::cout << "} ";
-		}
-		std::cout << '}';
-	}
-	if constexpr (std::is_same_v<T, std::unordered_map<char, set>>)
-	{
-		for (const auto& [v, s] : cont)
-		{
-			std::cout << v << ": { ";
-			for (int i : s)
-				std::cout << i << ' ';
-			std::cout << "}\n";
-		}
-	}
-	std::cout << '\n';
-}
-
 set operator& (const set& a, const set& b)
 {
 	set s;
@@ -89,7 +41,7 @@ set operator! (const set& a)
 	return s;
 }
 
-set& eval(std::deque<set>& s, char c)
+const set& process_operator(std::deque<set>& s, char c)
 {
 	if (c == '!')
 		return s.front() = !s.front();
@@ -101,9 +53,7 @@ set& eval(std::deque<set>& s, char c)
 		case '|': return s.emplace_front(a | b);
 		case '^': return s.emplace_front(a ^ b);
 		case '>': return s.emplace_front(!a | b);
-		case '=': if (a == b)
-					  return s.emplace_front(a);
-				  s.push_front({});
+		case '=': return s.emplace_front((a & b) | (!a & !b));
 	}
 	return U;
 }
@@ -148,57 +98,40 @@ bool is_operator(char c)
 std::vector<int> eval_set(std::string f, const std::vector<std::vector<int>>& sets)
 {
 	// if (!ast::validate(f))
-		// return std::cerr << "Invalid formula: " << f << '\n', std::vector<int>();
+	// {
+		// std::cerr << "Invalid formula: " << f << '\n';
+		// return {};
+	// }
 	// f = nnf::negation_normal_form(f);
 	auto vars = getVariables(f);
 	if (vars.size() != sets.size())
-		return std::cerr << "Number of variables does not match number of sets\n", std::vector<int>();
+	{
+		std::cerr << "Number of variables does not match number of sets\n";
+		return {};
+	}
+
 	U = buildUniverse(sets);
 
-	auto v = assignVariables(vars, sets);
-	std::deque<set> d;
-	for (char c : f)
-	{
-		if (std::isalpha(c))
-			d.emplace_back(v.at(c));
-		else if (is_operator(c))
-			eval(d, c);
-	}
-	/*
-	auto makeStack = [v = assignVariables(vars, sets)](auto&& acc, char c)
-	{
+	auto acc = [v = assignVariables(vars, sets)](auto&& acc, char c) {
 		if (std::isalpha(c))
 			acc.emplace_back(v.at(c));
+		else if (is_operator(c))
+			process_operator(acc, c);
 		return acc;
 	};
-	auto stack = std::accumulate(f.cbegin(), f.cend(), std::deque<set>(), makeStack);
+	auto result = std::accumulate(f.cbegin(), f.cend(), std::deque<set>(), acc).front();
 
-	auto eval_operators = [&stack](char c)
-	{
-		if (is_operator(c))
-			eval(stack, c);
-	};
-	std::for_each(f.cbegin(), f.cend(), eval_operators);
-
-	auto collect = [](auto&& acc, const set& s)
-	{
-		for (int i : s)
-			acc.emplace_back(i);
-		return acc;
-	};
-	return std::accumulate(stack.cbegin(), stack.cend(), std::vector<int>(), collect);
-	*/
-	auto collect = [](auto&& acc, const set& s)
-	{
-		for (int i : s)
-			acc.emplace_back(i);
-		return acc;
-	};
-	return std::accumulate(d.cbegin(), d.cend(), std::vector<int>(), collect);
+	return { result.begin(), result.end() };
 }
 
 void test(const std::string& s, const std::vector<std::vector<int>>& sets, const std::vector<int>& e)
 {
+	auto p = [](const std::vector<int>& v) {
+		std::cout << "{ ";
+		for (int i : v)
+			std::cout << i << ' ';
+		std::cout << "}\n";
+	};
 	auto r = eval_set(s, sets);
 	if (r != e)
 	{
