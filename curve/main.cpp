d2e9c86293ec33ec050d5d5331651f4eb4eb0cd3
George-Ber Zaltsman <berzaltsman@gmail.com>
2022-11-13 01:29:34 +0200

    added a "shortcut" for testing with python zCurve

--- curve/main.cpp
+++ curve/main.cpp
@@ -1,6 +1,13 @@
 #include "map.hpp"
 #include <iostream>
 
+/* pip install zCurve
+ *
+ * import zCurve as z
+ * def get_zcode(x, y):
+ *     return z.interlace(x, y, dims = 2, bits_per_bit = 16) / 4294977295
+ */
+
 int main()
 {
 	auto test = [](auto x, auto y)
