89e34e1b1d9e59b4890067b20cd43c9a310c3512
George-Ber Zaltsman <berzaltsman@gmail.com>
2022-11-13 14:59:40 +0200

    done

---
+++ inverse_function/reverse_map.cpp
@@ -0,0 +1,21 @@
+#include "reverse_map.hpp"
+
+namespace {
+
+	uint32_t deinterleave(uint32_t x)
+	{
+		x =  x             & 0b1010101010101010101010101010101;
+		x = (x | (x >> 1)) & 0b0110011001100110011001100110011;
+		x = (x | (x >> 2)) & 0b0001111000011110000111100001111;
+		x = (x | (x >> 4)) & 0b0000000111111110000000011111111;
+		x = (x | (x >> 8)) & 0b0000000000000001111111111111111;
+		return x;
+	}
+
+}
+
+std::pair<uint16_t, uint16_t> reverse_map(double n)
+{
+	uint32_t z = n * std::numeric_limits<uint32_t>::max();
+	return { deinterleave(z), deinterleave(z >> 1) };
+}
