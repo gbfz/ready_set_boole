2d48fd4cb7bd8e47c11945a21a4b7b5514f38f91
George-Ber Zaltsman <berzaltsman@gmail.com>
2022-12-25 21:52:03 +0200

    i had forgotten i didn't actually implement this particular exercise and had gone for a full AST implementation; now's a good time to push this! tests to come :)

---
+++ boolean_evaluation/boolean_evaluation.cpp
@@ -0,0 +1,12 @@
+#include "../ast/source/ASTree.hpp"
+
+bool eval_formula(const std::string& formula)
+{
+	std::optional<ast::tree> maybeTree = ast::generateTree(formula);
+	if (!maybeTree)
+	{
+		std::cerr << "Invalid formula: " << formula << '\n';
+		return {};
+	}
+	return maybeTree->exec();
+}
